cb29779ab864c559284d3690e9ac0d76bf6d773c
Danielle Madrid <danielle.madrid@edu.pucrs.br>
2026-07-02 03:18:38 +0000

    deletando mudanças antigss

--- demo/src/main/java/br/pucrs/daniellegabriella/demo/domain/enums/SituacaoJogo.java
+++
@@ -1,9 +0,0 @@
-package br.pucrs.daniellegabriella.demo.domain.enums;
-
-public enum SituacaoJogo {
-    DISPONIVEL,
-    CONTRATADO,
-    BLOQUEADO,
-    OBSOLETO,
-    REMOVIDO
-}
--- demo/src/main/java/br/pucrs/daniellegabriella/demo/infrastructure/repositories/RepositorioJpa.java
+++
@@ -1,8 +0,0 @@
-package br.pucrs.daniellegabriella.demo.infrastructure.repositories;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface RepositorioJpa extends CrudRepository<Cliente, Long>{
-
-}
